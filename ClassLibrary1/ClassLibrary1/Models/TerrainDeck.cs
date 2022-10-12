c069f6d73e72a2e0a34e3cf8766766042ef4769c
Camilla Jakobsen <s202750@dtu.dk>
2022-10-12 15:18:47 +0200

    Create Terrain Deck class

---
+++ ClassLibrary1/ClassLibrary1/Models/TerrainDeck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace ClassLibrary1.Models
+{
+
+    public class TerrainDeck
+    {
+        // Declaring variables
+        public int BIM7AATypeID { get; set; }
+        public string MaterialID { get; set; }
+        public double Area { get; set; }
+        public double Thickness { get; set; }
+        public Dictionary<string, string> MaterialProperties { get; set; } = new Dictionary<string, string>();
+        // In MaterialProperties is Density, strength, reinforcement ratio etc.
+
+
+        // Constructor: Starts with lower case letter
+        public TerrainDeck(int bIM7AATypeID, string materialID, double area, double thickness)
+        {
+            BIM7AATypeID = bIM7AATypeID;
+            MaterialID = materialID;
+            Area = area;
+            Thickness = thickness;
+
+        }
+
+
+    }
+}
