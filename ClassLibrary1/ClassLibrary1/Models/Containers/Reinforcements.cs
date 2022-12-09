dd6f6fc9f6bce80325184b9323ec2422d1104099
Camilla Jakobsen <s202750@dtu.dk>
2022-12-09 15:07:15 +0100

    Add reinforcements containers

---
+++ ClassLibrary1/ClassLibrary1/Models/Containers/Reinforcements.cs
@@ -0,0 +1,19 @@
+﻿using StructuralElementsExporter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models.Containers
+{
+    public class Reinforcements
+    {
+        public List<object> ReinforcementsInModel = new List<object>();
+
+        public void AddReinforcement(Reinforcement reinforcement)
+        {
+            ReinforcementsInModel.Add(reinforcement);
+        }
+    }
+}
